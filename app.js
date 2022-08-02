69d5adc5242f631fd307ea1fe837877fa658aafa
Lital-Rozenberg <109207977+litarozeberg@users.noreply.github.com>
2022-08-02 12:07:59 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const mongoose = require("mongoose")
+require("dotenv").config();
+const register = require("./routes/register")
+const login = require("./routes/login")
+const profile = require("./routes/profile")
+const cards = require("./routes/cards")
+
+const app = express()
+const PORT = process.env.PORT || 8000;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+mongoose
+    .connect(process.env.db, { useNewUrlParser: true })
+    .then(() => console.log("connect to MongoDB..."))
+    .catch(() => console.log("could not connect"));
+
+app.use("/api/register", register);
+app.use("/api/login", login);
+app.use("/api/profile", profile);
+app.use("/api/cards", cards);
+
+app.listen(PORT, () => console.log("Server started on port" + PORT));
